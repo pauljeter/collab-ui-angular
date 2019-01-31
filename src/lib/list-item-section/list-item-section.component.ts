85653456365ec88335d02ed47d85d878f792c8e9
Elena Giere <egiere@cisco.com>
2019-01-30 18:50:51 -0800

    added unit tests and cleaned up

--- src/lib/list-item-section/list-item-section.component.ts
+++ src/lib/list-item-section/list-item-section.component.ts
@@ -19,14 +19,12 @@ export class ListItemSectionComponent implements OnInit {
   @Input() class = '';
 
   ngOnInit() {
-    console.log(this.position);
     if (this.position && this.position !== 'left' && this.position !== 'center' && this.position !== 'center-align') {
       throw new Error('cui-list-item-section: ListItemSection position must be one of the following: left, center, right, center-align');
     }
   }
 
   setMyClasses() {
-    console.log('setMyClasses');
    const myClasses = `cui-list-item__${this.position}` +
     `${(this.class && ` ${this.class}`) || ''}`;
 

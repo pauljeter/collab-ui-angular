75288adfa2a73f25ff81129bd38d847b48873156
Elena Giere <egiere@cisco.com>
2019-01-30 18:50:51 -0800

    added unit tests and cleaned up

--- src/lib/list-item-section/list-item-section.component.spec.ts
+++ src/lib/list-item-section/list-item-section.component.spec.ts
@@ -1,10 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListItemSectionComponent } from './list-item-section.component';
 
 describe('ListItemSectionComponent', () => {
   let component: ListItemSectionComponent;
   let fixture: ComponentFixture<ListItemSectionComponent>;
+  let nativeElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +16,31 @@ describe('ListItemSectionComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListItemSectionComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should modify class with additional classes', () => {
+    component.class = 'myNewClass';
+    fixture.detectChanges();
+
+    nativeElement = fixture.nativeElement;
+    const listItemSection = nativeElement.querySelector('div');
+
+    expect(listItemSection).not.toBeNull();
+    expect(listItemSection.className).toEqual('cui-list-item__center myNewClass');
+  });
+
+  it('should modify class when position option is defined', () => {
+    component.position = 'left';
+    fixture.detectChanges();
+
+    nativeElement = fixture.nativeElement;
+    const listItemSection = nativeElement.querySelector('div');
+
+    expect(listItemSection).not.toBeNull();
+    expect(listItemSection.className).toEqual('cui-list-item__left');
   });
 });
